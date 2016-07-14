217b1c86e52529d6dd08bcce6b3c6be41d5a4249
ChalkPE <chalkpe@gmail.com>
2016-07-14 23:00:07 +0900

    Updated views

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cluster = require('cluster');
 var express = require('express');
 
+var vocaPackage = require('./package.json');
+
 if(cluster.isMaster){
     os.cpus().forEach(() => cluster.fork());
     cluster.on('exit', (worker, code, signal) => {
@@ -16,7 +18,7 @@ if(cluster.isMaster){
     app.set('view engine', 'pug');
     app.use(express.static(path.join(__dirname, 'static')));
 
-    require('./routes')(app);
+    require('./routes')(app, vocaPackage);
 
     app.listen(8080, () => console.log(`Listening on worker #${id}`))
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
-module.exports = app => {
-    app.get('/',         (req, res) => res.render('index'));
+module.exports = (app, vocaPackage) => {
+    app.get('/',         (req, res) => res.render('index', { version: vocaPackage.version }));
     app.get('/learn',    (req, res) => res.render('learn'));
     app.get('/exam',     (req, res) => res.render('exam'));
     app.get('/download', (req, res) => res.render('download'));

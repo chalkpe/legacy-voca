5de423536f15d245280425b6e1ec45897447960a
ChalkPE <chalkpe@gmail.com>
2016-07-18 23:00:12 +0900

    Hmm...

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 module.exports = (app, vocaPackage) => {
     app.get('/',         (req, res) => res.render('home', { version: vocaPackage.version }));
-    app.get('/learn',    (req, res) => res.render('learn'));
+    app.get('/learn',    (req, res) => res.render('learn', { book: 'wmr' }));
     app.get('/exam',     (req, res) => res.render('exam'));
     app.get('/download', (req, res) => res.render('download'));
 };

078bb3fdc26012fd5b722bdad3ca42618fd7bff9
ChalkPE <chalkpe@gmail.com>
2016-07-18 19:44:20 +0900

    Updated layout

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 module.exports = (app, vocaPackage) => {
-    app.get('/',         (req, res) => res.render('index', { version: vocaPackage.version }));
+    app.get('/',         (req, res) => res.render('home', { version: vocaPackage.version }));
     app.get('/learn',    (req, res) => res.render('learn'));
     app.get('/exam',     (req, res) => res.render('exam'));
     app.get('/download', (req, res) => res.render('download'));

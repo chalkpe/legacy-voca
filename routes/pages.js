353a3a039e4f95b78b3c8c3e4606d1f02a1797d7
ChalkPE <chalk@chalk.pe>
2016-09-06 23:02:12 +0900

    Implementing exam

--- routes/pages.js
+++ routes/pages.js
@@ -1,39 +1,60 @@
+const shuffle = require('shuffle-array');
+
 const Day = require('../models/Day');
 const Book = require('../models/Book');
 
-function findBooks(here){
+function findBooks(here, cb){
     return (req, res, next) => Book.find().sort('id').exec((err, books) => {
         if(err) return next(err);
         if(!books || !books.length) return next();
 
-        res.render('pages/books', { here, books });
+        cb(req, res, next, here, books);
     });
 }
 
-function findDays(here){
+function findDays(here, cb){
     return (req, res, next) => Day.count({ book: req.params.book }, (err, count) => {
         if(err) return next(err);
         if(!count) return next();
 
-        res.render('pages/days', { here, book: req.params.book, count });
+        cb(req, res, next, here, count);
     });
 }
 
-function findDay(here){
+function findDay(here, cb){
     return (req, res, next) => Day.findOne({ book: req.params.book, day: req.params.day }, (err, day) => {
         if(err) return next(err);
         if(!day) return next();
 
-        res.render('pages/day', { here, words: day.words });
+        cb(req, res, next, here, day);
     });
 }
 
-module.exports = (app) => {
-    app.get('/learn/:book/:day', findDay('/learn'));
-    app.get('/learn/:book', findDays('/learn'));
-    app.get('/learn', findBooks('/learn'));
+function renderBooks(req, res, next, here, books){
+    res.render('pages/books', { here, books });
+}
 
-    app.get('/exam/:book/:day', findDay('/exam'));
-    app.get('/exam/:book', findDays('/exam'));
-    app.get('/exam', findBooks('/exam'));
+function renderDays(req, res, next, here, count){
+    res.render('pages/days', { here, book: req.params.book, count });
+}
+
+function renderLearning(req, res, next, here, day){
+    res.render('pages/learning', { here, words: day.words });
+}
+
+function renderExamination(req, res, next, here, day){
+    let count = Math.min(day.words.length, 10);
+    let words = shuffle.pick(day.words, { picks: count });
+
+    res.render('pages/exam', { here, book: day.book, day: day.day, words });
+}
+
+module.exports = (app) => {
+    app.get('/learn/:book/:day', findDay('/learn', renderLearning));
+    app.get('/learn/:book',      findDays('/learn', renderDays));
+    app.get('/learn',            findBooks('/learn', renderBooks));
+
+    app.get('/exam/:book/:day', findDay('/exam', renderExamination));
+    app.get('/exam/:book',      findDays('/exam', renderDays));
+    app.get('/exam',            findBooks('/exam', renderBooks));
 };

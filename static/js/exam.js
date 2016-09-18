8619a489be0a857c1f722a691d9150bb197eead8
ChalkPE <chalk@chalk.pe>
2016-09-18 21:24:16 +0900

    Implemented client-side exam validation

--- static/js/exam.js
+++ static/js/exam.js
@@ -1,8 +1,21 @@
+function capitalize(text){
+    return text.replace(/\b\w/g , function(m){ return m.toUpperCase(); });
+}
+
 $(function(){
     $('.ui.rating').rating('disable');
     $('.ui.radio.checkbox').checkbox();
 
-    $('#submit').click(function(){
-        $('form').submit();
+    var inputs = {};
+    $('.ui.form').find('input:radio').each(function(){
+        var name = $(this).attr('name');
+
+        inputs[name] = {
+            identifier: name,
+            rules: [{ type: 'checked', prompt: 'The word "' + capitalize(name) + '" must be checked' }]
+        };
     });
+
+    $('.ui.form').form({ fields: inputs, on: 'blur' });
+    $('#submit').click(function(){ $('.ui.form').submit(); });
 });

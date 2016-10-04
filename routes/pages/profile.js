fffafb4a65f56c3186e453ee666db817941380dc
ChalkPE <chalk@chalk.pe>
2016-10-04 23:03:44 +0900

    Created app/profile

--- routes/pages/profile.js
+++ routes/pages/profile.js
@@ -1,8 +1,7 @@
-const moment = require('moment');
 const vocaPackage = require('../../package.json');
 
 const Book = require('../../models/Book');
-const Result = require('../../models/Result');
+const profile = require('../../app/profile');
 
 module.exports = Book.middleware('/', (req, res, next, here, books) => {
     res.locals.here = here;
@@ -10,22 +9,5 @@ module.exports = Book.middleware('/', (req, res, next, here, books) => {
     res.locals.vocaPackage = vocaPackage;
 
     if(!req.isAuthenticated()) next();
-    else Result.aggregate([
-        { $match: { user: req.user._id } },
-        { $group: { _id: { book: '$book', day: '$day' }, count: { $sum: 1 }, ids: { $push: '$_id' } } },
-        { $group: { _id: '$_id.book', days: { $push: { day: '$_id.day', count: '$count', ids: '$ids' } } } }
-    ], (err, results) => {
-        if(err) return next(err);
-
-        let r = {};
-        books.forEach(book => r[book.id] = []);
-        results.forEach(result => {
-            r[result._id].push(...result.days);
-            r[result._id].sort((a, b) => a.day - b.day);
-            r[result._id].forEach(day => day.dates = day.ids.map(id => moment(id.getTimestamp()).format('YYYY-MM-DD HH:mm:ss')).sort());
-        });
-
-        res.locals.results = r;
-        next();
-    });
+    else profile.getResults(req, res, next, books);
 });
